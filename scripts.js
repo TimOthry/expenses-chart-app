97c5a91a96de7e8354fd9a908f6f223285dcde2b
TimOthry <timsonlee3@gmail.com>
2023-12-19 23:39:01 +0000

    Implemented prototype hover function

--- scripts.js
+++ scripts.js
@@ -1,20 +1,29 @@
 function fetchData(filePath) {
   fetch(filePath)
-  .then(response => response.json())
-  .then(data => {
-    setSquares(data);
-  })
-  .catch(error => console.error('Error fetching JSON:', error));
+    .then(response => response.json())
+    .then(data => {
+      setSquares(data);
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
 }
 
 function setSquares(data) {
   for (items in data) {
     var squareDiv = document.getElementById(data[items]["day"]);
-    squareDiv.style.height =  parseFloat(data[items]["amount"]) * 2.2 + 'px';
+    var spending = data[items]["amount"];
+    squareDiv.style.height = parseFloat(spending) * 2.2 + 'px';
+    eventHandler(squareDiv, spending);
   }
   const now = new Date();
   var squareDiv = document.getElementById(data[now.getDay() - 1]["day"]);
   squareDiv.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+function eventHandler(squareDiv, spending) {
+  squareDiv.addEventListener("mouseover", () => {
+    squareDiv.style.backgroundColor = 'hsl(186, 34%, 60%)';
+    
+  });
 }
 
 fetchData('./data.json');
